e7906d05f5329947612b5f8a78f8aa0b4af4ce0c
Thiago <thiagoifnmgsi2015@gmail.com>
2024-10-18 12:12:02 -0300

    Adiciona copy no componente footer.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -46,6 +46,10 @@ function Footer() {
                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60935.714552369485!2d-44.96118740713735!3d-17.34051690438558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xaa0b7aa61b41d3%3A0xb54948d78e00fc4!2sPirapora%2C%20MG%2C%2039270-000!5e0!3m2!1spt-BR!2sbr!4v1729262168395!5m2!1spt-BR!2sbr"
                          width="100%" height="200" style={{border:0, borderRadius:9}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </Col>
+                {/* <Row className="text-center mt-3"> */}
+                    <Col className="text-center mt-3">
+                        <p className="custom-p">&copy; 2024 GetWeb. Todos os direitos reservados.</p>
+                    </Col>
             </Row>
         </Container>
     </footer>
